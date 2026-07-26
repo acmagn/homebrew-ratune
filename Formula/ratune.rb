ba224f88dc23dbc391ec5417a2aa52324ff7ad33
acmagn <acmagn@proton.me>
2026-07-26 01:17:25 -0600

    update to v0.15.2

--- Formula/ratune.rb
+++ Formula/ratune.rb
@@ -8,18 +8,18 @@
 class Ratune < Formula
   desc "Terminal music player for Subsonic-compatible servers"
   homepage "https://github.com/acmagn/ratune"
-  version "0.15.1"
+  version "0.15.2"
   license "MIT"
   depends_on :macos
 
   on_arm do
     url "https://github.com/acmagn/ratune/releases/download/v#{version}/ratune-v#{version}-aarch64-apple-darwin.tar.gz"
-    sha256 "9f0ca13cb5f5e895adf6a6c3fc57f2a060ca3961526bb21c558bb25d179c321b"
+    sha256 "203d3cec1037504b3b50f3ae2c0b167b31bc0a92248af2ff38c1083e4d09802d"
   end
 
   on_intel do
     url "https://github.com/acmagn/ratune/releases/download/v#{version}/ratune-v#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "5889db19beff9f2f3a2bc3102340781c62481c366b81c1f328b0adc8539575ed"
+    sha256 "a64015749d3b48c013098ffe517bdcf41dd9901567951026fae2d1219a5e25cb"
   end
 
   def install
